1fe0d59029f1cd368ae8613d986459fc14714630
theCreator19 <77932869+theCreator19@users.noreply.github.com>
2021-06-12 19:13:58 +0530

    Create String Split and Join.py

---
+++ String Split and Join.py
@@ -0,0 +1,12 @@
+# String Split and Join
+# HackerRank Problem solved
+def split_and_join(line):
+    # write your code here
+    line=line.split()
+    line="-".join(line)
+    return line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
