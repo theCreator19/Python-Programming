bf7f3f334ee0e1d319ad76eb727e9f64043fc290
theCreator19 <77932869+theCreator19@users.noreply.github.com>
2021-06-13 23:59:22 +0530

    Create Symmetric Difference.py

---
+++ Symmetric Difference.py
@@ -0,0 +1,16 @@
+# Symmetric Difference
+# HackerRank Problem Solved
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num1=int(input())
+set1=input().split()
+set1=set(set1)
+num2=int(input())
+set2=input().split()
+set2=set(set2)
+sd=set1.symmetric_difference(set2)
+sd=list(sd)
+for i in range(len(sd)):
+    sd[i]=int(sd[i])
+sd=sorted(sd)
+for i in range(len(sd)):
+    print(sd[i])
